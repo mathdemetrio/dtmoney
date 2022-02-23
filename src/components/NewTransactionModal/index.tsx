0ed25f0b951f2ddbb5c4359ea86b4ffda2b917f9
Matheus Demetrio <matheus.fdemetrio@gmail.com>
2022-02-23 20:28:15 -0300

    Form styles

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,8 @@
 import Modal from "react-modal"
-import { Container } from "./styles";
+import closeImg from "../../assets/close.svg"
+import incomeImg from "../../assets/income.svg"
+import outcomeImg from "../../assets/outcome.svg"
+import { Container, TransactionTypeContainer } from "./styles";
 
 Modal.setAppElement('#root');
 
@@ -16,12 +19,32 @@ export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionMod
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button type="button" onClick={onRequestClose} className="react-modal-close">
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+
       <Container>
         <h2>Cadastrar Transação</h2>
 
         <input type="text" placeholder="Título" />
 
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button 
+            type="button"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button 
+            type="button"
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
 
         <input type="text" placeholder="Categoria" />
 
