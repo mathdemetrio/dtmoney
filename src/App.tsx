85b8503cd626554235fd06a6d8c59242910e489b
Matheus Demetrio <matheus.fdemetrio@gmail.com>
2022-03-02 20:24:28 -0300

    consume transactions API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { createServer } from "miragejs";
 import { useState } from "react";
 
 import { Dashboard } from "./components/Dashboard";
@@ -6,24 +5,6 @@ import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal"
 import { GlobalStyle } from "./styles/global";
 
-createServer({
-  routes() {
-    this.namespace = 'api'
-
-    this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
-    })
-  }
-})
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import Modal from "react-modal"
 import closeImg from "../../assets/close.svg"
 import incomeImg from "../../assets/income.svg"
 import outcomeImg from "../../assets/outcome.svg"
+import { api } from "../../services/api";
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
 Modal.setAppElement('#root');
@@ -13,7 +14,23 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionModalProps) {
+  const [title, setTitle] = useState('')
+  const [value, setValue] = useState(0)
+  const [category, setCategory] = useState('')
   const [type, setType] = useState('deposit')
+
+  function handleCreateNewTransaction(event:FormEvent) {
+    event.preventDefault()
+
+    const data = {
+      title,
+      value,
+      category,
+      type
+    }
+
+    api.post('transactions', data)
+  }
 
   return (
     <Modal 
@@ -26,12 +43,22 @@ export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionMod
         <img src={closeImg} alt="Fechar modal" />
       </button>
 
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar Transação</h2>
 
-        <input type="text" placeholder="Título" />
+        <input 
+          type="text" 
+          placeholder="Título" 
+          value={title}
+          onChange={event => setTitle(event.target.value)}  
+        />
 
-        <input type="number" placeholder="Valor" />
+        <input 
+          type="number" 
+          placeholder="Valor" 
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}  
+        />
 
         <TransactionTypeContainer>
           <RadioBox 
@@ -55,7 +82,12 @@ export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionMod
           </RadioBox>
         </TransactionTypeContainer>
 
-        <input type="text" placeholder="Categoria" />
+        <input 
+          type="text" 
+          placeholder="Categoria" 
+          value={category}
+          onChange={event => setCategory(event.target.value)}    
+        />
 
         <button type="submit">
           Cadastrar
