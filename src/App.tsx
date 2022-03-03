0895464901edc4f38260abb4a63c26f2889c7f84
Matheus Demetrio <matheus.fdemetrio@gmail.com>
2022-03-02 21:56:56 -0300

    create transactions context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { TransactionsProvider } from "./hooks/useTransactions"
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
@@ -18,7 +19,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal 
@@ -26,6 +27,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
